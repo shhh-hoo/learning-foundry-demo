999f10396b1b1f53886484f6dc8dd511bb36bc9e
Andrew_Time <34838425+shhh-hoo@users.noreply.github.com>
2026-07-17 21:05:01 +0800

    Load external component catalog styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./surfaces.css";
+import "./external-components.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
